452ee9bf84c28cbb2732359a29471438cc9d38e1
leandro barroso <lrbarroso1984@gmail.com>
2020-08-17 16:33:27 -0300

    Challenge 05-01-03 - longest word game - complete solution - needs refactoring

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,7 +9,7 @@ class GamesController < ApplicationController
     @word = params[:word]
 
     word_check = helpers.check_dictionary(@word)
-    @result = "Congratulations!#{@word} is a valid English word!"
+    @result = "Congratulations! #{@word} is a valid English word!"
     word_letters = @word.upcase.chars
     @result = "Sorry but #{@word} can't be buit out of #{@letters}" unless word_letters.all? { |l| @letters.count(l) >= word_letters.count(l) }
     @result = "Sorry but #{@word} does not seem to be a valid English word" unless word_check['found']
